3bec5b1c3306e694a15884e64137e4bb32c53e3f
Nicola Di Pol <nicola.dipol@vonage.com>
2021-10-19 23:39:40 +0100

    Purely functional Stack: WIP

--- src/main/scala/Stack.scala
+++ src/main/scala/Stack.scala
@@ -1,19 +1,31 @@
 // Model a purely functional Stack
 
-sealed trait Stack[T]
+sealed trait Stack[T] {
 
-case class StackDefault[T](elements: List[T]) extends Stack[T]
+  def toList: List[T] = this match {
+    case EmptyStack() => List.empty
+    case NonEmptyStack(top, tail) => top :: tail.toList
+  }
+
+  override def toString: String = {
+    val bodyStr = this.toList.foldLeft("")((s, t) => if (s.isEmpty) s"$t" else s"$s > $t")
+    s"[ ${bodyStr} ]"
+  }
+}
+
+case class EmptyStack[T]() extends Stack[T]
+case class NonEmptyStack[T](top: T, tail: Stack[T]) extends Stack[T]
 
 object Stack {
-  def push[T]: T => Stack[T] => Stack[T] = e => {
-    case StackDefault(elements) => StackDefault(e +: elements)
+
+  def push[T]: T => Stack[T] => Stack[T] = t => {
+    case EmptyStack() => NonEmptyStack(t, EmptyStack())
+    case s@NonEmptyStack(_, _) => NonEmptyStack(t, s)
   }
 
   def pop[T]: Stack[T] => Option[(T, Stack[T])] = {
-    case StackDefault(elements) => elements match {
-      case ::(head, next) => Some((head, StackDefault(next)))
-      case Nil => None
-    }
+    case EmptyStack() => None
+    case NonEmptyStack(top, tail) => Some((top, tail))
   }
 
   // implement with tail recursion
--- src/test/scala/StackSpec.scala
+++ src/test/scala/StackSpec.scala
@@ -1,22 +1,39 @@
 import Stack.{pop, push}
-import org.scalacheck.Gen.{listOf, listOfN, negNum, oneOf, posNum}
-import org.scalacheck.Prop.{forAll, propBoolean}
+import org.scalacheck.Gen.{const, listOf, listOfN, lzy, negNum, oneOf, posNum}
+import org.scalacheck.Prop.{collect, forAll, propBoolean}
 import org.scalacheck.{Gen, Properties}
-
-import scala.Function.uncurried
 
 object StackSpec extends Properties("Stack") {
 
   val elementGen: Gen[Int] = oneOf(posNum[Int], negNum[Int])
-  val elementsToPushGen: Gen[List[Int]] = posNum[Int].flatMap(listOfN(_, elementGen))
-  val stackGen: Gen[Stack[Int]] = listOf(elementGen).map(StackDefault(_))
+  val elementListGen: Gen[List[Int]] = posNum[Int].flatMap(listOfN(_, elementGen))
+  val emptyStackGen: Gen[EmptyStack[Int]] = const(EmptyStack())
+//  val nonEmptyStackGen: Gen[NonEmptyStack[Int]] =
+//    for { t <- elementGen; s <- stackGen } yield NonEmptyStack(t, s)
+  val nonEmptyStackGen: Gen[NonEmptyStack[Int]] =
+    elementGen.flatMap(e => stackGen.map(NonEmptyStack(e, _)))
+//  val stackGen: Gen[Stack[Int]] = lzy(oneOf(emptyStackGen, nonEmptyStackGen))
+  def stackFromList[T](ts: List[T]): Stack[T] =
+    ts.foldLeft(EmptyStack(): Stack[T])((s, t) => NonEmptyStack(t, s))
+  val stackGen: Gen[Stack[Int]] = listOf(elementGen).map(stackFromList)
 
   property("n pushes followed by n pops leave the Stack unchanged") =
-    forAll (elementsToPushGen, stackGen) { (elementsToPush: List[Int], stack: Stack[Int]) =>
-      val stackAfterNPushes = elementsToPush.foldRight(stack)(uncurried(push))
-      val stackAfterNPushesAndNPops = elementsToPush.foldRight(stackAfterNPushes)((_, s) =>
-        pop(s).map(_._2).getOrElse(StackDefault(List[Int]()))
+    forAll (elementListGen, stackGen) { (ts, stack) =>
+      val stackAfterPushes = ts.foldLeft(stack)((s, t) => push(t)(s))
+      val stackAfterPushesAndPops = ts.indices.foldLeft(stackAfterPushes)((s, _) =>
+        pop(s).map(_._2).getOrElse(EmptyStack())
       )
-      stack == stackAfterNPushesAndNPops
+      stack == stackAfterPushesAndPops
+    }
+
+  property("pushing all elements of a list in a stack then popping them out produces the same list") =
+    forAll (elementListGen, stackGen) { (ts, stack) =>
+      val stackAfterPushes = ts.foldLeft(stack)((s, t) => push(t)(s))
+      val poppedList = ts.indices.foldLeft((List[Int](), stackAfterPushes))((res, _) =>
+        pop(res._2) match {
+          case Some((top, tail)) => (top :: res._1, tail)
+          case None => (res._1, EmptyStack())
+        })._1
+      ts == poppedList
     }
 }
