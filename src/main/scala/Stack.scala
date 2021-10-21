2678deb46e062aed91f0b553eb3ad3ce57cbbe65
Alessandro Candolini <alessandrocandolini@gmail.com>
2021-10-21 03:52:46 +0100

    batch operations and covariance

--- src/main/scala/Stack.scala
+++ src/main/scala/Stack.scala
@@ -2,14 +2,14 @@ import scala.annotation.tailrec
 
 // Model a purely functional Stack
 
-sealed trait Stack[T] {
+sealed trait Stack[+T] {
 
   def toList: List[T] = {
 
     @tailrec
     def toListAux(acc : List[T], s : Stack[T]) : List[T] =
       s match {
-        case EmptyStack() => acc
+        case EmptyStack => acc
         case NonEmptyStack(top, tail) => toListAux( top :: acc, tail)
       }
 
@@ -23,21 +23,30 @@ sealed trait Stack[T] {
   }
 }
 
-case class EmptyStack[T]() extends Stack[T]
-case class NonEmptyStack[T](top: T, tail: Stack[T]) extends Stack[T]
+case object EmptyStack extends Stack[Nothing]
+case class NonEmptyStack[+T](top: T, tail: Stack[T]) extends Stack[T]
 
 object Stack {
 
-  def push[T]: T => Stack[T] => Stack[T] = t => s => NonEmptyStack(t, s)
+  def push[T, V >: T]: V => Stack[T] => Stack[V] = v => s => NonEmptyStack(v, s)
 
   def pop[T]: Stack[T] => Option[(T, Stack[T])] = {
-    case EmptyStack() => None
+    case EmptyStack => None
     case NonEmptyStack(top, tail) => Some((top, tail))
   }
 
+  // insert N elements starting from the leftmost
+  def pushBatch[T, V >: T]: List[V] => Stack[T] => Stack[V] = vs => stack =>
+    vs.foldLeft[Stack[V]](stack) ( (s, v) => push(v)(s) )
+
+  def popBatch[T]: Int => Stack[T] => (List[T], Stack[T]) = n => stack =>
+    List.fill(n)(()).foldLeft((List.empty[T], stack)) {
+      case ((ts, s), _) => pop(s).map { case (t, s1) => (ts :+ t, s1) }.getOrElse((ts, EmptyStack))
+    }
+
   def min[T : Ordering](s : Stack[T]): Option[T] = s match {
-      case EmptyStack() => None
-      case NonEmptyStack(top, EmptyStack()) => Some(top)
+      case EmptyStack => None
+      case NonEmptyStack(top, EmptyStack) => Some(top)
       case NonEmptyStack(top, NonEmptyStack(sndTop, tail)) =>
         val topMin = Ordering[T].min(top, sndTop)
         min(tail).map(Ordering[T].min(topMin, _)).orElse(Some(topMin))
@@ -47,14 +56,14 @@ object Stack {
     @tailrec
     def aux(acc: T, s: Stack[T]): T =
       s match {
-        case EmptyStack() => acc
+        case EmptyStack => acc
         case NonEmptyStack(top, tail) =>
           val newAcc = Ordering[T].min(acc, top)
           aux(newAcc, tail)
       }
 
     s match {
-      case EmptyStack() => None
+      case EmptyStack => None
       case NonEmptyStack(top, tail) => Some(aux(top, tail))
     }
 
@@ -65,22 +74,28 @@ object Stack {
     @tailrec
     def aux(acc: V, s: Stack[T]): V =
       s match {
-        case EmptyStack() => acc
+        case EmptyStack => acc
         case NonEmptyStack(top, tail) =>
           aux(combine(acc, top), tail)
       }
 
     s match {
-      case EmptyStack() => v
+      case EmptyStack => v
       case NonEmptyStack(top, tail) => aux(combine(v,top), tail)
     }
   }
 
   def foldMin[T: Ordering](s : Stack[T]): Option[T] = {
     s match {
-      case EmptyStack() => None
+      case EmptyStack => None
       case NonEmptyStack(top, tail) =>  Some(fold(tail, top, Ordering[T].min[T]))
     }
+  }
+
+  def size[T]: Stack[T] => Int = s => {
+    fold[T, Int](s, 0, {
+      case (n,_) => n +1
+    })
   }
 
 }
--- src/test/scala/StackSpec.scala
+++ src/test/scala/StackSpec.scala
@@ -1,26 +1,20 @@
-import Stack.{foldMin, min, pop, push, safeMin}
-import org.scalacheck.Gen.{const, listOf, listOfN, negNum, oneOf, posNum}
+import Stack._
+import org.scalacheck.Gen.{listOf, negNum, oneOf, posNum}
 import org.scalacheck.Prop.{forAll, propBoolean}
 import org.scalacheck.{Gen, Properties}
 
 object StackSpec extends Properties("Stack") {
 
   val elementGen: Gen[Int] = oneOf(posNum[Int], negNum[Int])
-  val elementListGen: Gen[List[Int]] = posNum[Int].flatMap(listOfN(_, elementGen))
+  val elementListGen: Gen[List[Int]] = listOf(elementGen)
 
-  val emptyStackGen: Gen[EmptyStack[Int]] = const(EmptyStack())
-//  val nonEmptyStackGen: Gen[NonEmptyStack[Int]] =
-//    for { t <- elementGen; s <- stackGen } yield NonEmptyStack(t, s)
-  val nonEmptyStackGen: Gen[NonEmptyStack[Int]] =
-    elementGen.flatMap(e => stackGen.map(NonEmptyStack(e, _)))
-//  val stackGen: Gen[Stack[Int]] = lzy(oneOf(emptyStackGen, nonEmptyStackGen))
-  val stackGen: Gen[Stack[Int]] = listOf(elementGen).map(stackFromList)
+  val stackGen : Gen[Stack[Int]] = elementListGen.map(stackFromList)
 
-  property("n pushes followed by n pops leave the Stack unchanged") =
+  property("n pushes followed by n pops leave any Stack unchanged") =
     forAll (elementListGen, stackGen) { (ts, stack) =>
       val stackAfterPushes = ts.foldLeft(stack)((s, t) => push(t)(s))
-      val stackAfterPushesAndPops = ts.indices.foldLeft(stackAfterPushes)((s, _) =>
-        pop(s).map(_._2).getOrElse(EmptyStack())
+      val stackAfterPushesAndPops = ts.foldLeft(stackAfterPushes)((s, _) =>
+        pop(s).map(_._2).getOrElse(EmptyStack)
       )
       stack == stackAfterPushesAndPops
     }
@@ -28,10 +22,10 @@ object StackSpec extends Properties("Stack") {
   property("pushing all elements of a list in a stack then popping them out produces the same list") =
     forAll (elementListGen, stackGen) { (ts, stack) =>
       val stackAfterPushes = ts.foldLeft(stack)((s, t) => push(t)(s))
-      val poppedList = ts.indices.foldLeft((List[Int](), stackAfterPushes))((res, _) =>
+      val poppedList = ts.foldLeft((List[Int](), stackAfterPushes))((res, _) =>
         pop(res._2) match {
           case Some((top, tail)) => (top :: res._1, tail)
-          case None => (res._1, EmptyStack())
+          case None => (res._1, EmptyStack)
         })._1
       ts == poppedList
     }
@@ -48,6 +42,39 @@ object StackSpec extends Properties("Stack") {
   property("safeMin and foldMin return the same result") =
     forAll (stackGen) { s => safeMin(s) == foldMin(s) }
 
+  property("fromList and toList should produce same list") =
+    forAll { s : List[Int] => stackFromList(s).toList == s }
+
+  property("pushBatch of one element is the same as push") =
+    forAll (elementGen, stackGen) { (t, stack) =>
+      pushBatch(List(t))(stack) == push(t)(stack)
+    }
+
+  property("popBatch once is the same as pop") =
+    forAll (elementGen, stackGen) { (t, stack) =>
+      pushBatch(List(t))(stack) == push(t)(stack)
+    }
+
+  property("pushBatch and popBatch in sequence return the same stack and the same list in reverse order ") =
+    forAll (elementListGen, stackGen) { (ts, stack) =>
+      val r = popBatch(ts.length)(pushBatch(ts)(stack))
+      r._2 == stack && r._1 == ts.reverse
+    }
+
+  property("two sequential pushBatch are the same as pushBatch of the concatenation of two lists") =
+    forAll (elementListGen, elementListGen, stackGen) { (ts1, ts2, stack) =>
+      val s1 = pushBatch(ts1)(stack)
+      val s2 = pushBatch(ts2)(s1)
+      s2 == pushBatch(ts1 ++ ts2)(stack)
+    }
+
+  property("popBatch a number of times equal to the size o the stack should always return empty stack") =
+    forAll (stackGen) { stack =>
+      val n = size(stack)
+      val r = popBatch(n)(stack)
+      r._2 == EmptyStack && r._1.length == n
+    }
+
   def stackFromList[T](ts: List[T]): Stack[T] =
-    ts.foldLeft(EmptyStack(): Stack[T])((s, t) => NonEmptyStack(t, s))
+    ts.foldLeft[Stack[T]](EmptyStack)((s, t) => NonEmptyStack(t, s))
 }
