9ac0f46e5d58834f5a8f52760d2f79137e7b22ce
horothesun <horothesun@gmail.com>
2023-03-14 01:07:14 +0000

    ClassicTetris: WIP

--- src/main/scala/ClassicTetris.scala
+++ src/main/scala/ClassicTetris.scala
@@ -22,27 +22,9 @@ object ClassicTetris {
     def max(x: Height, y: Height): Height = if (x.value >= y.value) x else y
   }
 
-  case class Dimensions(width: Width, height: Height)
-
-  def transpose[A](rows: List[List[A]]): List[List[A]] = {
-    def heads(rows: List[List[A]]): List[A] = rows.map(_.head)
-    def tails(rows: List[List[A]]): List[List[A]] = rows.map(_.tail)
-    @tailrec
-    def aux(acc: List[List[A]], rows: List[List[A]]): List[List[A]] =
-      rows match {
-        case Nil => acc
-        case firstRow :: _ =>
-          firstRow match {
-            case Nil    => acc
-            case _ :: _ => aux(heads(rows) +: acc, tails(rows))
-          }
-      }
-
-    aux(acc = List.empty, rows)
-  }
-
   trait Shape[A] {
-    val dims: Dimensions
+    val width: Width
+    val height: Height
     def render: List[List[Option[A]]]
 
     def show(filled: A => String, hole: => String): String =
@@ -52,16 +34,19 @@ object ClassicTetris {
 
     def empty[A]: Shape[A] = MonoidK[Shape].empty
     def hole[A]: Shape[A] = new Shape[A] {
-      override lazy val dims: Dimensions = Dimensions(Width(1), Height(1))
+      override lazy val width: Width = Width(1)
+      override lazy val height: Height = Height(1)
       override def render: List[List[Option[A]]] = List(List(None))
     }
     def filled[A](a: A): Shape[A] = new Shape[A] {
-      override lazy val dims: Dimensions = Dimensions(Width(1), Height(1))
+      override lazy val width: Width = Width(1)
+      override lazy val height: Height = Height(1)
       override def render: List[List[Option[A]]] = List(List(Some(a)))
     }
 
     def rotatedCW[A](s: Shape[A]): Shape[A] = new Shape[A] {
-      override lazy val dims: Dimensions = Dimensions(Width(s.dims.height.value), Height(s.dims.width.value))
+      override lazy val width: Width = Width(s.height.value)
+      override lazy val height: Height = Height(s.width.value)
       override def render: List[List[Option[A]]] = transpose(s.render).map(_.reverse).reverse
     }
 
@@ -74,29 +59,44 @@ object ClassicTetris {
 
     def topBottom[A](t: Shape[A], bs: Shape[A]*): Shape[A] = if (bs.isEmpty) t else bs.fold(t)(topBottom)
 
+    def transpose[A](rows: List[List[A]]): List[List[A]] = {
+      def heads(rows: List[List[A]]): List[A] = rows.map(_.head)
+      def tails(rows: List[List[A]]): List[List[A]] = rows.map(_.tail)
+      @tailrec
+      def aux(acc: List[List[A]], rows: List[List[A]]): List[List[A]] =
+        rows match {
+          case Nil => acc
+          case firstRow :: _ =>
+            firstRow match {
+              case Nil    => acc
+              case _ :: _ => aux(heads(rows) +: acc, tails(rows))
+            }
+        }
+      aux(acc = List.empty, rows)
+    }
+
     implicit val horizontalMonoidK: MonoidK[Shape] = new MonoidK[Shape] {
       override def empty[A]: Shape[A] = new Shape[A] {
-        override lazy val dims: Dimensions = Dimensions(Width(0), Height(0))
+        override lazy val width: Width = Width(0)
+        override lazy val height: Height = Height(0)
         override def render: List[List[Option[A]]] = List.empty
       }
       override def combineK[A](x: Shape[A], y: Shape[A]): Shape[A] = new Shape[A] {
-        override lazy val dims: Dimensions = Dimensions(
-          x.dims.width + y.dims.width,
-          Height.max(x.dims.height, y.dims.height)
-        )
+        override lazy val width: Width = x.width + y.width
+        override lazy val height: Height = Height.max(x.height, y.height)
         override def render: List[List[Option[A]]] = {
           val renderedX =
-            if (x.dims.height >= y.dims.height) x.render
+            if (x.height >= y.height) x.render
             else {
-              val numbOfMissingRows = (y.dims.height - x.dims.height).value
-              val missingRow = List.fill[Option[A]](x.dims.width.value)(None)
+              val numbOfMissingRows = (y.height - x.height).value
+              val missingRow = List.fill[Option[A]](x.width.value)(None)
               x.render ++ List.fill(numbOfMissingRows)(missingRow)
             }
           val renderedY =
-            if (y.dims.height >= x.dims.height) y.render
+            if (y.height >= x.height) y.render
             else {
-              val numbOfMissingRows = (x.dims.height - y.dims.height).value
-              val missingRow = List.fill[Option[A]](y.dims.width.value)(None)
+              val numbOfMissingRows = (x.height - y.height).value
+              val missingRow = List.fill[Option[A]](y.width.value)(None)
               y.render ++ List.fill(numbOfMissingRows)(missingRow)
             }
           renderedX.zip(renderedY).map { case (xRow, yRow) => xRow ++ yRow }
@@ -117,60 +117,38 @@ object ClassicTetris {
   val fh = beside(f, h)
   val ff = beside(f, f)
   val fff = beside(f, f, f)
+  val fhf = beside(f, h, f)
   val hfh = beside(h, f, h)
   val hff = beside(h, f, f)
   val ffh = beside(f, f, h)
   val i = beside(f, f, f, f)
-  val o = topBottom(
-    ff,
-    ff
-  )
-  val t = topBottom(
-    fff,
-    hfh
-  )
-  val j = topBottom(
-    hf,
-    hf,
-    hf,
-    ff
-  )
-  val l = topBottom(
-    fh,
-    fh,
-    fh,
-    ff
-  )
-  val s = topBottom(
-    hff,
-    ffh
-  )
-  val z = topBottom(
-    ffh,
-    hff
-  )
-  val plus = topBottom(
-    hfh,
-    fff,
-    hfh
-  )
+  val o = topBottom(ff, ff)
+  val t = topBottom(fff, hfh)
+  val j = topBottom(hf, hf, hf, ff)
+  val l = topBottom(fh, fh, fh, ff)
+  val s = topBottom(hff, ffh)
+  val z = topBottom(ffh, hff)
+  val plus = topBottom(hfh, fff, hfh)
+  val times = topBottom(fhf, hfh, fhf)
+  val diamond = topBottom(hfh, fhf, hfh)
+  val emptySquare = topBottom(fff, fhf, fff)
 
-  def showEmptyGrid(hole: => String, ds: Dimensions): String = {
+  def showEmptyGrid(hole: => String, width: Width, height: Height): String = {
     val leftBorder = "<!"
     val rightBorder = "!>"
-    val emptyRow = List.fill(ds.width.value)(hole).mkString(leftBorder, "", rightBorder)
-    val bottomBorder = List.fill(ds.width.value)("==").mkString(leftBorder, "", rightBorder) ++
-      "\n" ++ List.fill(ds.width.value)("\\/").mkString("  ", "", "  ")
-    (List.fill(ds.height.value)(emptyRow) :+ bottomBorder).mkString("\n")
+    val emptyRow = List.fill(width.value)(hole).mkString(leftBorder, "", rightBorder)
+    val bottomBorder = List.fill(width.value)("==").mkString(leftBorder, "", rightBorder) ++
+      "\n" ++ List.fill(width.value)("\\/").mkString("  ", "", "  ")
+    (List.fill(height.value)(emptyRow) :+ bottomBorder).mkString("\n")
   }
 
   def main(args: Array[String]): Unit = {
-    println(showEmptyGrid(hole = " .", Dimensions(Width(10), Height(20))))
+    println(showEmptyGrid(hole = " .", Width(10), Height(20)))
 
     println("\n---")
 
     println(
-      List(i, o, t, j, l, s, z, plus)
+      List(i, o, t, j, l, s, z, plus, times, diamond, emptySquare)
         .map(_.show(filled = _ => "🟩", hole = "⬜️"))
         .mkString("\n", "\n\n---\n\n", "\n")
     )
