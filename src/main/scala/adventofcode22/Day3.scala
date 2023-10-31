d7d65cca648e79c15e79f01ac0ef27eb4788499f
horothesun <horothesun@gmail.com>
2023-10-31 01:17:50 +0000

    AoC 2022: day 3

---
+++ src/main/scala/adventofcode22/Day3.scala
@@ -0,0 +1,65 @@
+package adventofcode22
+
+import adventofcode22.Day3.Item._
+import cats.data.NonEmptyList
+import cats.implicits._
+
+object Day3 {
+
+  case class Item(c: Char) {
+    def priority: Int =
+      if (LOWERCASE_CHARS.contains(c)) c.toInt - 96
+      else if (UPPERCASE_CHARS.contains(c)) c.toInt - 38
+      else 0
+  }
+  object Item {
+    private val LOWERCASE_CHARS = 'a' to 'z'
+    private val UPPERCASE_CHARS = 'A' to 'Z'
+
+    def from(c: Char): Option[Item] =
+      if ((LOWERCASE_CHARS ++ UPPERCASE_CHARS).contains(c)) Some(Item(c))
+      else None
+  }
+
+  case class Rucksack(firstHalf: NonEmptyList[Item], secondHalf: NonEmptyList[Item]) {
+
+    def repeatedItemsBetweenCompartments: List[Item] = firstHalf.toList.intersect(secondHalf.toList)
+
+    def allItems: NonEmptyList[Item] = firstHalf.concatNel(secondHalf)
+
+  }
+  object Rucksack {
+    def from(s: String): Option[Rucksack] =
+      if (s.length % 2 != 0) None
+      else {
+        val (l, r) = s.splitAt(s.length / 2)
+        (l.toList, r.toList) match {
+          case (lh :: lt, rh :: rt) =>
+            (
+              NonEmptyList(lh, lt).traverse(Item.from),
+              NonEmptyList(rh, rt).traverse(Item.from)
+            ).mapN(Rucksack.apply)
+          case _ => None
+        }
+      }
+  }
+
+  def parseRucksacks(input: List[String]): Option[List[Rucksack]] = input.traverse(Rucksack.from)
+
+  def getRepeatedItemsPrioritiesSum(input: List[String]): Option[Int] =
+    parseRucksacks(input).map(_.mapFilter(_.repeatedItemsBetweenCompartments.headOption).map(_.priority).sum)
+
+  def getItemsPresentInAllRucksacks(rs: List[Rucksack]): List[Item] = rs.map(_.allItems.toList).reduce(_ intersect _)
+
+  def getGroupedBadgeItemsPrioritiesSum(input: List[String]): Option[Int] =
+    parseRucksacks(input)
+      .map(_.grouped(3).toList.mapFilter(g => getItemsPresentInAllRucksacks(g).headOption).map(_.priority).sum)
+
+  def getLinesFromFile(filename: String): List[String] = {
+    val source = scala.io.Source.fromFile(filename)
+    val result = source.getLines.toList
+    source.close
+    result
+  }
+
+}
---
+++ src/test/scala/adventofcode22/Day3Suite.scala
@@ -0,0 +1,80 @@
+package adventofcode22
+
+import adventofcode22.Day3._
+import cats.data.NonEmptyList
+import cats.implicits._
+import munit.ScalaCheckSuite
+
+class Day3Suite extends ScalaCheckSuite {
+
+  test("Item('a') to Item('z') priorities are (1 to 26)") {
+    val items = ('a' to 'z').toList.traverse(Item.from)
+    assertEquals(items.map(_.map(_.priority)), Some((1 to 26).toList))
+  }
+
+  test("Item('A') to Item('Z') priorities are (27 to 52)") {
+    val items = ('A' to 'Z').toList.traverse(Item.from)
+    assertEquals(items.map(_.map(_.priority)), Some((27 to 52).toList))
+  }
+
+  test("Rucksack.from(\"\") returns None") {
+    assertEquals(Rucksack.from(""), None)
+  }
+
+  test("Rucksack.from(\"aBC\") returns None") {
+    assertEquals(Rucksack.from("aBC"), None)
+  }
+
+  test("Rucksack.from(\"abcDEF\") returns a valid Rucksack") {
+    assertEquals(
+      Rucksack.from("abcDEF"),
+      Some(
+        Rucksack(
+          firstHalf = NonEmptyList.of(Item('a'), Item('b'), Item('c')),
+          secondHalf = NonEmptyList.of(Item('D'), Item('E'), Item('F'))
+        )
+      )
+    )
+  }
+
+  test("Rucksack repeatedItemsBetweenCompartments with NO repeated") {
+    assertEquals(
+      Rucksack(
+        firstHalf = NonEmptyList.of(Item('a'), Item('b'), Item('c')),
+        secondHalf = NonEmptyList.of(Item('D'), Item('A'), Item('F'))
+      ).repeatedItemsBetweenCompartments,
+      List.empty
+    )
+  }
+
+  test("Rucksack repeatedItemsBetweenCompartments with 1 repeated") {
+    assertEquals(
+      Rucksack(
+        firstHalf = NonEmptyList.of(Item('a'), Item('b'), Item('c')),
+        secondHalf = NonEmptyList.of(Item('D'), Item('a'), Item('F'))
+      ).repeatedItemsBetweenCompartments,
+      List(Item('a'))
+    )
+  }
+
+  test("Rucksack repeatedItemsBetweenCompartments with 2 repeated") {
+    assertEquals(
+      Rucksack(
+        firstHalf = NonEmptyList.of(Item('a'), Item('b'), Item('c')),
+        secondHalf = NonEmptyList.of(Item('c'), Item('C'), Item('a'))
+      ).repeatedItemsBetweenCompartments,
+      List(Item('a'), Item('c'))
+    )
+  }
+
+  test("getRepeatedItemsPrioritiesSum on BIG input") {
+    val input = getLinesFromFile("src/test/scala/adventofcode22/day3_input.txt")
+    assertEquals(getRepeatedItemsPrioritiesSum(input), Some(7_903))
+  }
+
+  test("getGroupedBadgeItemsPrioritiesSum on BIG input") {
+    val input = getLinesFromFile("src/test/scala/adventofcode22/day3_input.txt")
+    assertEquals(getGroupedBadgeItemsPrioritiesSum(input), Some(2_548))
+  }
+
+}
