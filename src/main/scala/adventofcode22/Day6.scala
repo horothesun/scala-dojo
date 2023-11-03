52ea522264bc63aca26c9ef68051cd629c2265d6
horothesun <horothesun@gmail.com>
2023-11-03 00:42:49 +0000

    AoC 2022: day 6

---
+++ src/main/scala/adventofcode22/Day6.scala
@@ -0,0 +1,36 @@
+package adventofcode22
+
+import cats.data.NonEmptyList
+import cats.implicits._
+import cats.Order
+
+object Day6 {
+
+  def areAllDifferent[A: Order](as: NonEmptyList[A]): Boolean = as.toNes.size == as.length
+
+  def getPrefixAndFinal0BasedIndex[A: Order](maxPrefixLength: Int, as: List[A]): Option[(List[A], Int)] =
+    as.zipWithIndex
+      .scanLeft[(List[A], Int)]((List.empty, -1)) { case ((acc, _), (c, i)) =>
+        ((acc :+ c).takeRight(maxPrefixLength), i)
+      }
+      .find { case (prefixSoFar, _) =>
+        prefixSoFar.length == maxPrefixLength &&
+        prefixSoFar.toNel.exists(areAllDifferent)
+      }
+
+  def getFinal1BasedIndex[A: Order](maxPrefixLength: Int, as: List[A]): Option[Int] =
+    getPrefixAndFinal0BasedIndex(maxPrefixLength, as).map { case (_, i) => i + 1 }
+
+  def parseChars(input: List[String]): Option[List[Char]] =
+    input match {
+      case firstLine :: Nil => Some(firstLine.toList)
+      case _                => None
+    }
+
+  def getStartOfPacketMarkerIndex(input: List[String]): Option[Int] =
+    parseChars(input).flatMap(cs => getFinal1BasedIndex(maxPrefixLength = 4, cs))
+
+  def getStartOfMessageMarkerIndex(input: List[String]): Option[Int] =
+    parseChars(input).flatMap(cs => getFinal1BasedIndex(maxPrefixLength = 14, cs))
+
+}
---
+++ src/test/scala/adventofcode22/Day6Suite.scala
@@ -0,0 +1,96 @@
+package adventofcode22
+
+import cats.data.NonEmptySet
+import cats.implicits._
+import munit.ScalaCheckSuite
+import org.scalacheck.Gen
+import org.scalacheck.Prop._
+import Day6._
+import Day6Suite._
+import FileLoader._
+
+class Day6Suite extends ScalaCheckSuite {
+
+  property("areAllDifferent on unique non-empty list is always true") {
+    forAll(nonEmptySetGen)(nes => assert(areAllDifferent(nes.toNonEmptyList)))
+  }
+
+  test("getPrefixAndFinal0BasedIndex") {
+    assertEquals(
+      getPrefixAndFinal0BasedIndex(maxPrefixLength = 3, List('a', 'b', 'a', 'd', 'e')),
+      Some((List('b', 'a', 'd'), 3))
+    )
+  }
+
+  test("getFinal1BasedIndex(maxPrefixLength = 4, \"mjqjpqmgbljsphdztnvjfqwrcgsmlb\".toList) returns Some(7)") {
+    val input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb".toList
+    assertEquals(getFinal1BasedIndex(maxPrefixLength = 4, input), Some(7))
+  }
+
+  test("getFinal1BasedIndex(maxPrefixLength = 4, \"bvwbjplbgvbhsrlpgdmjqwftvncz\".toList) returns Some(5)") {
+    val input = "bvwbjplbgvbhsrlpgdmjqwftvncz".toList
+    assertEquals(getFinal1BasedIndex(maxPrefixLength = 4, input), Some(5))
+  }
+
+  test("getFinal1BasedIndex(maxPrefixLength = 4, \"nppdvjthqldpwncqszvftbrmjlhg\".toList) returns Some(5)") {
+    val input = "nppdvjthqldpwncqszvftbrmjlhg".toList
+    assertEquals(getFinal1BasedIndex(maxPrefixLength = 4, input), Some(6))
+  }
+
+  test("getFinal1BasedIndex(maxPrefixLength = 4, \"nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg\".toList) returns Some(10)") {
+    val input = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".toList
+    assertEquals(getFinal1BasedIndex(maxPrefixLength = 4, input), Some(10))
+  }
+
+  test("getFinal1BasedIndex(maxPrefixLength = 4, \"zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw\".toList) returns Some(11)") {
+    val input = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".toList
+    assertEquals(getFinal1BasedIndex(maxPrefixLength = 4, input), Some(11))
+  }
+
+  test("getStartOfPacketMarkerIndex (BIG input)") {
+    val input = getLinesFromFile("src/test/scala/adventofcode22/day6_input.txt")
+    assertEquals(getStartOfPacketMarkerIndex(input), Some(1850))
+  }
+
+  test("getFinal1BasedIndex(maxPrefixLength = 14, \"mjqjpqmgbljsphdztnvjfqwrcgsmlb\".toList) returns Some(19)") {
+    val input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb".toList
+    assertEquals(getFinal1BasedIndex(maxPrefixLength = 14, input), Some(19))
+  }
+
+  test("getFinal1BasedIndex(maxPrefixLength = 14, \"bvwbjplbgvbhsrlpgdmjqwftvncz\".toList) returns Some(23)") {
+    val input = "bvwbjplbgvbhsrlpgdmjqwftvncz".toList
+    assertEquals(getFinal1BasedIndex(maxPrefixLength = 14, input), Some(23))
+  }
+
+  test("getFinal1BasedIndex(maxPrefixLength = 14, \"nppdvjthqldpwncqszvftbrmjlhg\".toList) returns Some(23)") {
+    val input = "nppdvjthqldpwncqszvftbrmjlhg".toList
+    assertEquals(getFinal1BasedIndex(maxPrefixLength = 14, input), Some(23))
+  }
+
+  test("getFinal1BasedIndex(maxPrefixLength = 14, \"nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg\".toList) returns Some(29)") {
+    val input = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".toList
+    assertEquals(getFinal1BasedIndex(maxPrefixLength = 14, input), Some(29))
+  }
+
+  test("getFinal1BasedIndex(maxPrefixLength = 14, \"zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw\".toList) returns Some(26)") {
+    val input = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".toList
+    assertEquals(getFinal1BasedIndex(maxPrefixLength = 14, input), Some(26))
+  }
+
+  test("getStartOfMessageMarkerIndex (BIG input)") {
+    val input = getLinesFromFile("src/test/scala/adventofcode22/day6_input.txt")
+    assertEquals(getStartOfMessageMarkerIndex(input), Some(2823))
+  }
+
+}
+object Day6Suite {
+
+  val nonEmptySetGen: Gen[NonEmptySet[Int]] =
+    Gen
+      .zip(
+        Gen.posNum[Int],
+        Gen.listOf(Gen.posNum[Int])
+      )
+      .map { case (i, is) => NonEmptySet.of(i, is: _*) }
+
+}
