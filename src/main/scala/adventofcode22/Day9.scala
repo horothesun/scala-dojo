0b8cf0e8b8377683c87d46fbc32067b6975fdb57
horothesun <horothesun@gmail.com>
2023-11-19 18:44:51 +0000

    AoC 2022: day 9 part 1

--- src/main/scala/adventofcode22/Day9.scala
+++ src/main/scala/adventofcode22/Day9.scala
@@ -5,6 +5,7 @@ import cats.data.NonEmptyList
 import cats.derived
 import cats.implicits._
 import scala.annotation.tailrec
+import Day9.Proximity._
 import Day9.Rope._
 
 object Day9 {
@@ -24,6 +25,7 @@ object Day9 {
     case class Knot[A](a: A) extends Rope[A]
     case class Segment[A](head: Knot[A], tail: Rope[A]) extends Rope[A]
 
+    implicit val functor: Functor[Rope] = derived.semiauto.functor
     implicit val foldable: Foldable[Rope] = derived.semiauto.foldable
 
     def make2[A](a: A): Rope[A] = fromNel(NonEmptyList.of(a, a))
@@ -35,25 +37,145 @@ object Day9 {
           case Nil     => acc
           case a :: as => aux(Segment(head = Knot(a), tail = acc), as)
         }
+
       val NonEmptyList(reversedHead, reversedTail) = nel.reverse
       aux(acc = Knot(reversedHead), rest = reversedTail)
     }
 
   }
 
+  sealed trait Direction
+  object Direction {
+    case object Left extends Direction
+    case object Right extends Direction
+    case object Up extends Direction
+    case object Down extends Direction
+
+    def values: Array[Direction] = Array(Left, Right, Up, Down)
+
+    def from(s: String): Option[Direction] =
+      s match {
+        case "L" => Some(Left)
+        case "R" => Some(Right)
+        case "U" => Some(Up)
+        case "D" => Some(Down)
+        case _   => None
+      }
+  }
+
+  sealed trait Proximity
+  object Proximity {
+    case object Adjacent extends Proximity
+    case object Distant extends Proximity
+  }
+
   case class Pos(x: Int, y: Int) {
 
-    def left: Pos = Pos(x - 1, y)
-    def right: Pos = Pos(x + 1, y)
-    def up: Pos = Pos(x, y + 1)
-    def down: Pos = Pos(x, y - 1)
+    val move: Direction => Pos = {
+      case Direction.Left  => Pos(x - 1, y)
+      case Direction.Right => Pos(x + 1, y)
+      case Direction.Up    => Pos(x, y + 1)
+      case Direction.Down  => Pos(x, y - 1)
+    }
 
-    // TODO: test!!! 🔥🔥🔥
-    def isAdjacentTo(that: Pos): Boolean = (Math.abs(x - that.x) <= 1) && (Math.abs(y - that.y) <= 1)
+    def move(n: Int, d: Direction): Pos = List.fill[Pos => Pos](n)(_.move(d)).foldMap(identity).apply(this)
+
+    def getProximity(that: Pos): Proximity =
+      if ((Math.abs(x - that.x) <= 1) && (Math.abs(y - that.y) <= 1)) Adjacent else Distant
+
+    def getNewFollowerPos(leader: Pos): Pos =
+      getProximity(leader) match {
+        case Adjacent => this
+        case Distant =>
+          if (x == leader.x) {
+            Pos(
+              x,
+              leader.y + (if (y < leader.y) -1 else 1)
+            )
+          } else if (y == leader.y) {
+            Pos(
+              leader.x + (if (x < leader.x) -1 else 1),
+              y
+            )
+          } else {
+            if (Math.abs(x - leader.x) > Math.abs(y - leader.y)) {
+              Pos(
+                leader.x + (if (x < leader.x) -1 else 1),
+                leader.y
+              )
+            } else {
+              Pos(
+                leader.x,
+                leader.y + (if (y < leader.y) -1 else 1)
+              )
+            }
+          }
+      }
 
   }
   object Pos {
+
+    implicit val posToPosMonoid: Monoid[Pos => Pos] = new Monoid[Pos => Pos] {
+      override def empty: Pos => Pos = identity
+      override def combine(x: Pos => Pos, y: Pos => Pos): Pos => Pos = p => x(y(p))
+    }
+
     val start: Pos = Pos(0, 0)
+
   }
+
+  case class Motion(direction: Direction, steps: Int)
+  object Motion {
+    def from(s: String): Option[Motion] =
+      s.split(" ") match {
+        case Array(motion, steps) =>
+          (
+            Direction.from(motion),
+            steps.toIntOption
+          ).mapN(Motion.apply)
+      }
+  }
+
+  def getMotions(input: List[String]): Option[List[Motion]] = input.traverse(Motion.from)
+
+  def getSingleSteps(motions: List[Motion]): List[Direction] =
+    motions.flatMap { case Motion(direction, steps) => List.fill[Direction](steps)(direction) }
+
+  def getRopeAfterHeadMoves(direction: Direction, rope: Rope[Pos]): Rope[Pos] = {
+    val newHeadPos = rope.getHead.a.move(direction)
+    rope match {
+      case Knot(_) => Knot(newHeadPos)
+      case Segment(_, tail) =>
+        Rope.fromNel(
+          tail
+            .foldLeft((newHeadPos, NonEmptyList.one(newHeadPos))) { case ((newLeaderPos, acc), p) =>
+              val newFollowerPos = p.getNewFollowerPos(newLeaderPos)
+              (newFollowerPos, newFollowerPos :: acc)
+            }
+            ._2
+            .reverse
+        )
+    }
+  }
+
+  def getAllRopes(singleStepMotions: List[Direction], initialRope: Rope[Pos]): List[Rope[Pos]] =
+    singleStepMotions
+      .foldLeft[(Rope[Pos], List[Rope[Pos]])]((initialRope, List(initialRope))) { case ((prevRope, acc), d) =>
+        val newRope = getRopeAfterHeadMoves(d, prevRope)
+        (newRope, newRope :: acc)
+      }
+      ._2
+
+  def getDistinctRopeTailPositionCount(motions: List[Motion]): Int =
+    getAllRopes(
+      singleStepMotions = getSingleSteps(motions),
+      initialRope = Rope.make2(Pos.start)
+    )
+      .map(_.getLast.a)
+      .toSet
+      .size
+
+  def getDistinctRopeTailPositionCount(input: List[String]): Option[Int] =
+    getMotions(input).map(getDistinctRopeTailPositionCount)
 
 }
--- src/test/scala/adventofcode22/Day9Suite.scala
+++ src/test/scala/adventofcode22/Day9Suite.scala
@@ -2,7 +2,10 @@ package adventofcode22
 
 import cats.data.NonEmptyList
 import munit.ScalaCheckSuite
+import org.scalacheck.Gen
+import org.scalacheck.Prop._
 import Day9._
+import Day9.Proximity._
 import Day9.Rope._
 import Day9Suite._
 
@@ -29,9 +32,263 @@ class Day9Suite extends ScalaCheckSuite {
     assertEquals(rope.getLast, Knot(3))
   }
 
+  property("Pos p is adjacent to p.move(d) for any p and d") {
+    forAll(positionGen, directionGen) { case (p, d) =>
+      assertEquals(p.getProximity(p.move(d)), Adjacent)
+    }
+  }
+
+  property("Pos p is distant from p.move(n, d) for any p, d and n >= 2") {
+    forAll(positionGen, Gen.chooseNum(2, 100), directionGen) { case (p, n, d) =>
+      assertEquals(p.getProximity(p.move(n, d)), Distant)
+    }
+  }
+
+  test("Pos(1, 1).getNewFollowerPos(Pos(3, 1)) == Pos(2, 1)") {
+    assertEquals(Pos(1, 1).getNewFollowerPos(Pos(3, 1)), Pos(2, 1))
+  }
+
+  test("Pos(1, 3).getNewFollowerPos(Pos(1, 1)) == Pos(1, 2)") {
+    assertEquals(Pos(1, 3).getNewFollowerPos(Pos(1, 1)), Pos(1, 2))
+  }
+
+  test("Pos(1, 1).getNewFollowerPos(Pos(2, 3)) == Pos(2, 2)") {
+    assertEquals(Pos(1, 1).getNewFollowerPos(Pos(2, 3)), Pos(2, 2))
+  }
+
+  test("Pos(1, 1).getNewFollowerPos(Pos(3, 2)) == Pos(2, 2)") {
+    assertEquals(Pos(1, 1).getNewFollowerPos(Pos(3, 2)), Pos(2, 2))
+  }
+
+  property("p.getNewFollowerPos(p.move(d)) == p for any Pos p and Direction d") {
+    forAll(positionGen, directionGen) { case (p, d) =>
+      assertEquals(p.getNewFollowerPos(p.move(d)), p)
+    }
+  }
+
+  property("f.getNewFollowerPos(h) is adjacent to h for any Pos f and h") {
+    forAll(positionGen, positionGen) { case (f, h) =>
+      assertEquals(f.getNewFollowerPos(h).getProximity(h), Adjacent)
+    }
+  }
+
+  test("parse motions (small input)") {
+    val input =
+      """
+        |R 4
+        |U 4
+        |L 3
+        |D 1
+        |R 4
+        |D 1
+        |L 5
+        |R 2
+        |""".stripMargin.linesIterator.toList.drop(1)
+    assertEquals(getMotions(input), Some(motions))
+  }
+
+  test("getSingleSteps returns correct value (small input)") {
+    assertEquals(
+      getSingleSteps(motions),
+      List(
+        Direction.Right,
+        Direction.Right,
+        Direction.Right,
+        Direction.Right,
+        Direction.Up,
+        Direction.Up,
+        Direction.Up,
+        Direction.Up,
+        Direction.Left,
+        Direction.Left,
+        Direction.Left,
+        Direction.Down,
+        Direction.Right,
+        Direction.Right,
+        Direction.Right,
+        Direction.Right,
+        Direction.Down,
+        Direction.Left,
+        Direction.Left,
+        Direction.Left,
+        Direction.Left,
+        Direction.Left,
+        Direction.Right,
+        Direction.Right
+      )
+    )
+  }
+
+  property("getSingleSteps preserves total number of single steps for any Motion list") {
+    forAll(Gen.listOf(motionGen)) { motions =>
+      assertEquals(getSingleSteps(motions).length, motions.map(_.steps).sum)
+    }
+  }
+
+  test("getRopeAfterHeadMoves(Right, H(0,0)~T(0,0)) == H(1,0)~T(0,0)") {
+    val rope: Rope[Pos] = Rope.make2(Pos.start)
+    assertEquals(
+      getRopeAfterHeadMoves(Direction.Right, rope),
+      Segment(head = Knot(Pos(1, 0)), tail = Knot(Pos(0, 0)))
+    )
+  }
+
+  test("getRopeAfterHeadMoves(Right, H(1,0)~T(0,0)) == H(2,0)~T(1,0)") {
+    val rope: Rope[Pos] = Segment(head = Knot(Pos(1, 0)), tail = Knot(Pos(0, 0)))
+    assertEquals(
+      getRopeAfterHeadMoves(Direction.Right, rope),
+      Segment(head = Knot(Pos(2, 0)), tail = Knot(Pos(1, 0)))
+    )
+  }
+
+  test("getRopeAfterHeadMoves(Right, H(2,0)~T(1,0)) == H(3,0)~T(2,0)") {
+    val rope: Rope[Pos] = Segment(head = Knot(Pos(2, 0)), tail = Knot(Pos(1, 0)))
+    assertEquals(
+      getRopeAfterHeadMoves(Direction.Right, rope),
+      Segment(head = Knot(Pos(3, 0)), tail = Knot(Pos(2, 0)))
+    )
+  }
+
+  test("getRopeAfterHeadMoves(Right, H(3,0)~T(2,0)) == H(4,0)~T(3,0)") {
+    val rope: Rope[Pos] = Segment(head = Knot(Pos(3, 0)), tail = Knot(Pos(2, 0)))
+    assertEquals(
+      getRopeAfterHeadMoves(Direction.Right, rope),
+      Segment(head = Knot(Pos(4, 0)), tail = Knot(Pos(3, 0)))
+    )
+  }
+
+  test("getRopeAfterHeadMoves(Up, H(4,0)~T(3,0)) == H(4,1)~T(3,0)") {
+    val rope: Rope[Pos] = Segment(head = Knot(Pos(4, 0)), tail = Knot(Pos(3, 0)))
+    assertEquals(
+      getRopeAfterHeadMoves(Direction.Up, rope),
+      Segment(head = Knot(Pos(4, 1)), tail = Knot(Pos(3, 0)))
+    )
+  }
+
+  test("getRopeAfterHeadMoves(Up, H(4,1)~T(3,0)) == H(4,2)~T(4,1)") {
+    val rope: Rope[Pos] = Segment(head = Knot(Pos(4, 1)), tail = Knot(Pos(3, 0)))
+    assertEquals(
+      getRopeAfterHeadMoves(Direction.Up, rope),
+      Segment(head = Knot(Pos(4, 2)), tail = Knot(Pos(4, 1)))
+    )
+  }
+
+  test("getRopeAfterHeadMoves(Up, H(4,2)~T(4,1)) == H(4,3)~T(4,2)") {
+    val rope: Rope[Pos] = Segment(head = Knot(Pos(4, 2)), tail = Knot(Pos(4, 1)))
+    assertEquals(
+      getRopeAfterHeadMoves(Direction.Up, rope),
+      Segment(head = Knot(Pos(4, 3)), tail = Knot(Pos(4, 2)))
+    )
+  }
+
+  test("getRopeAfterHeadMoves(Up, H(4,3)~T(4,2)) == H(4,4)~T(4,3)") {
+    val rope: Rope[Pos] = Segment(head = Knot(Pos(4, 3)), tail = Knot(Pos(4, 2)))
+    assertEquals(
+      getRopeAfterHeadMoves(Direction.Up, rope),
+      Segment(head = Knot(Pos(4, 4)), tail = Knot(Pos(4, 3)))
+    )
+  }
+
+  test("getRopeAfterHeadMoves(Left, H(4,4)~T(4,3)) == H(3,4)~T(4,3)") {
+    val rope: Rope[Pos] = Segment(head = Knot(Pos(4, 4)), tail = Knot(Pos(4, 3)))
+    assertEquals(
+      getRopeAfterHeadMoves(Direction.Left, rope),
+      Segment(head = Knot(Pos(3, 4)), tail = Knot(Pos(4, 3)))
+    )
+  }
+
+  test("getRopeAfterHeadMoves(Left, H(3,4)~T(4,3)) == H(2,4)~T(3,4)") {
+    val rope: Rope[Pos] = Segment(head = Knot(Pos(3, 4)), tail = Knot(Pos(4, 3)))
+    assertEquals(
+      getRopeAfterHeadMoves(Direction.Left, rope),
+      Segment(head = Knot(Pos(2, 4)), tail = Knot(Pos(3, 4)))
+    )
+  }
+
+  test("getRopeAfterHeadMoves(Left, H(2,4)~T(3,4)) == H(1,4)~T(2,4)") {
+    val rope: Rope[Pos] = Segment(head = Knot(Pos(2, 4)), tail = Knot(Pos(3, 4)))
+    assertEquals(
+      getRopeAfterHeadMoves(Direction.Left, rope),
+      Segment(head = Knot(Pos(1, 4)), tail = Knot(Pos(2, 4)))
+    )
+  }
+
+  test("getRopeAfterHeadMoves(Down, H(1,4)~T(2,4)) == H(1,3)~T(2,4)") {
+    val rope: Rope[Pos] = Segment(head = Knot(Pos(1, 4)), tail = Knot(Pos(2, 4)))
+    assertEquals(
+      getRopeAfterHeadMoves(Direction.Down, rope),
+      Segment(head = Knot(Pos(1, 3)), tail = Knot(Pos(2, 4)))
+    )
+  }
+
+  test("getRopeAfterHeadMoves(Right, H(1,3)~T(2,4)) == H(2,3)~T(2,4)") {
+    val rope: Rope[Pos] = Segment(head = Knot(Pos(1, 3)), tail = Knot(Pos(2, 4)))
+    assertEquals(
+      getRopeAfterHeadMoves(Direction.Right, rope),
+      Segment(head = Knot(Pos(2, 3)), tail = Knot(Pos(2, 4)))
+    )
+  }
+
+  test("getRopeAfterHeadMoves(Right, H(2,3)~T(2,4)) == H(3,3)~T(2,4)") {
+    val rope: Rope[Pos] = Segment(head = Knot(Pos(2, 3)), tail = Knot(Pos(2, 4)))
+    assertEquals(
+      getRopeAfterHeadMoves(Direction.Right, rope),
+      Segment(head = Knot(Pos(3, 3)), tail = Knot(Pos(2, 4)))
+    )
+  }
+
+  test("getRopeAfterHeadMoves(Right, H(3,3)~T(2,4)) == H(4,3)~T(3,3)") {
+    val rope: Rope[Pos] = Segment(head = Knot(Pos(3, 3)), tail = Knot(Pos(2, 4)))
+    assertEquals(
+      getRopeAfterHeadMoves(Direction.Right, rope),
+      Segment(head = Knot(Pos(4, 3)), tail = Knot(Pos(3, 3)))
+    )
+  }
+
+  test("getRopeAfterHeadMoves(Right, H(4,3)~T(3,3)) == H(5,3)~T(4,3)") {
+    val rope: Rope[Pos] = Segment(head = Knot(Pos(4, 3)), tail = Knot(Pos(3, 3)))
+    assertEquals(
+      getRopeAfterHeadMoves(Direction.Right, rope),
+      Segment(head = Knot(Pos(5, 3)), tail = Knot(Pos(4, 3)))
+    )
+  }
+
+  test("getDistinctRopeTailPositionCount(motions) == 13 (small input)") {
+    assertEquals(getDistinctRopeTailPositionCount(motions), 13)
+  }
+
+  test("getDistinctRopeTailPositionCount(bigInput) == Some(6284)") {
+    assertEquals(getDistinctRopeTailPositionCount(bigInput), Some(6284))
+  }
+
 }
 object Day9Suite {
 
-//  val bigInput: List[String] = FileLoader.getLinesFromFile("src/test/scala/adventofcode22/day9_input.txt")
+  val bigInput: List[String] = FileLoader.getLinesFromFile("src/test/scala/adventofcode22/day9_input.txt")
+
+  val motions: List[Motion] = List(
+    Motion(Direction.Right, 4),
+    Motion(Direction.Up, 4),
+    Motion(Direction.Left, 3),
+    Motion(Direction.Down, 1),
+    Motion(Direction.Right, 4),
+    Motion(Direction.Down, 1),
+    Motion(Direction.Left, 5),
+    Motion(Direction.Right, 2)
+  )
+
+  def ropeGen[A](aGen: Gen[A]): Gen[Rope[A]] = Gen.lzy(Gen.oneOf(knotGen(aGen), segmentGen(aGen)))
+  def knotGen[A](aGen: Gen[A]): Gen[Knot[A]] = aGen.map(Knot.apply)
+  def segmentGen[A](aGen: Gen[A]): Gen[Segment[A]] =
+    Gen.zip(knotGen(aGen), ropeGen(aGen)).map { case (k, r) => Segment(head = k, tail = r) }
+
+  def directionGen: Gen[Direction] = Gen.oneOf(Direction.values.toSeq)
+
+  def positionGen: Gen[Pos] = {
+    val intGen = Gen.chooseNum(-100, 100)
+    Gen.zip(intGen, intGen).map { case (x, y) => Pos(x, y) }
+  }
+
+  def motionGen: Gen[Motion] = Gen.zip(directionGen, Gen.posNum[Int]).map((Motion.apply _).tupled)
 
 }
