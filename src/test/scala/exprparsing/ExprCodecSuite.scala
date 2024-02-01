3a3db262823a1e74d175b23d493a5409ca69a663
horothesun <horothesun@gmail.com>
2024-02-01 23:00:40 +0000

    ExprParsing: parser tests

--- src/test/scala/exprparsing/ExprCodecSuite.scala
+++ src/test/scala/exprparsing/ExprCodecSuite.scala
@@ -1,14 +1,13 @@
 package exprparsing
+
 import ExprCodec._
 import ExprCodecSuite._
 import ExprEval._
 import ExprGenerators._
 import Models._
 import Models.EvalError._
-import Models.Expr._
 import Models.ExprR._
 import Models.Factor._
-import Models.Term._
 import Models.TermR._
 import Models.Unary._
 import munit.Assertions._
@@ -19,7 +18,71 @@ class ExprCodecSuite extends ScalaCheckSuite {
 
   /* parser */
 
-  // TODO: ...
+  test("\"1\" parses to Natural(1)") {
+    assertFullyParsed(numericP.parse("1"), Natural(1))
+  }
+
+  test("\"1.0\" parses to NonNegDecimal(1.0)") {
+    assertFullyParsed(numericP.parse("1.0"), NonNegDecimal(1.0))
+  }
+
+  test("\"1\" parses to Expr.numb(1)") {
+    assertFullyParsed(exprP.parse("1"), Expr.numb(1))
+  }
+
+  test("\"1.0\" parses to Expr.numb(1.0)") {
+    assertFullyParsed(exprP.parse("1.0"), Expr.numb(1.0))
+  }
+
+  test("\"1+2\" parses to Expr(1, Add(2, ε))") {
+    assertFullyParsed(exprP.parse("1+2"), Expr(Term.numb(1), Add(Term.numb(2), EEpsilon)))
+  }
+
+  test("\"(42)\" parses to Grouped(Natural(42))") {
+    assertFullyParsed(exprP.parse("(42)"), Expr.grouped(Expr.numb(42)))
+  }
+
+  test("\"(6/3)\" parses to Grouped(Div(6, 3))") {
+    assertFullyParsed(
+      exprP.parse("(6/3)"),
+      Expr.grouped(Expr(Term(Factor.numb(6), Div(Factor.numb(3), TEpsilon)), EEpsilon))
+    )
+  }
+
+  test("\"1+2*3\" parses to Expr(1, Add(Term(2, Mul(3, ε)), ε))") {
+    assertFullyParsed(
+      exprP.parse("1+2*3"),
+      Expr(Term.numb(1), Add(Term(Factor.numb(2), Mul(Factor.numb(3), TEpsilon)), EEpsilon))
+    )
+  }
+
+  test("\"1+6/3\" parses to Expr(1, Add(Term(6, Div(3, ε)), ε))") {
+    assertFullyParsed(
+      exprP.parse("1+6/3"),
+      Expr(Term.numb(1), Add(Term(Factor.numb(6), Div(Factor.numb(3), TEpsilon)), EEpsilon))
+    )
+  }
+
+  test("\"2^3^2\" parses to Expr(Term(Pow(2, Pow(3, 2)), ε), ε)") {
+    assertFullyParsed(
+      exprP.parse("2^3^2"),
+      Expr(Term(Pow(Power.numb(2), Pow(Power.numb(3), Factor.numb(2))), TEpsilon), EEpsilon)
+    )
+  }
+
+  test("\"2^3^4^5\" parses to Expr(Term(Pow(2, Pow(3, Pow(4, 5))), ε), ε)") {
+    assertFullyParsed(
+      exprP.parse("2^3^4^5"),
+      Expr(Term(Pow(Power.numb(2), Pow(Power.numb(3), Pow(Power.numb(4), Factor.numb(5)))), TEpsilon), EEpsilon)
+    )
+  }
+
+  test("\"1-2.0+3-4.0\" parses to Expr(1, Sub(2.0, Add(3, Sub(4.0, ε))))") {
+    assertFullyParsed(
+      exprP.parse("1-2.0+3-4.0"),
+      Expr(Term.numb(1), Sub(Term.numb(2.0), Add(Term.numb(3), Sub(Term.numb(4.0), EEpsilon))))
+    )
+  }
 
   /* encode */
 
